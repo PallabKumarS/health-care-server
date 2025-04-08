37610fae6405da9d1611c12321b0cbf58948277f
PallabKumarS <pallabkumar26@gmail.com>
2025-04-08 22:06:44 +0600

    started doing a prisma project

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import app from "./app";
+
+import { Server } from "http";
+import config from "./app/config";
+
+let server: Server;
+
+async function main() {
+  try {
+    // connect to database
+   
+
+    server = app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
+      console.log(`🚀 Server is running successfully! 🚀`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected, closing server...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected, closing server...", err);
+  process.exit(1);
+});
